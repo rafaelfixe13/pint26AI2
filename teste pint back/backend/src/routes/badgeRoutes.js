0d97a4a0cd043d13c5ad13c525d201c69a383181
SimaoAlm <simaoalm45@gmail.com>
2026-04-11 12:10:51 +0100

    Add getBadgeById and deleteBadge routes

--- teste pint back/backend/src/routes/badgeRoutes.js
+++ teste pint back/backend/src/routes/badgeRoutes.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllBadges,
+  getBadgeById,
   createBadge,
   updateBadge,
+  deleteBadge,
 } = require('../controllers/badgeController');
 
 router.get('/', getAllBadges);
+router.get('/:id', getBadgeById);
 router.post('/', createBadge);
 router.put('/:id', updateBadge);
+router.delete('/:id', deleteBadge);
 
 module.exports = router;
