bc19d0331df3337a001b447384cbd11e31340eba
costa <costagoncalo056@gmail.com>
2026-05-18 14:29:30 +0100

    Add files via upload

--- teste pint back/backend/src/routes/tmRoutes.js
+++ teste pint back/backend/src/routes/tmRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   listarBadgesEspeciais,
   listarRanking,
+  listarConsultores,
 } = require('../controllers/tmController');
 
 router.get('/badgesespeciais', listarBadgesEspeciais);
 router.get('/ranking', listarRanking);
+router.get('/consultores', listarConsultores);
 
 module.exports = router;
